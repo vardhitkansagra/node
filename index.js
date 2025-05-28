be7af38d2f0be586c5c0af65046d4ab5280fc9bb
vardhit kansagra <vardhitkansagra3@gmail.com>
2025-05-28 16:45:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From dhruv" }');
+    res.send('{ "response": "Hello From vardhit" }');
 });
 
 app.get('/will', function (req, res) {
